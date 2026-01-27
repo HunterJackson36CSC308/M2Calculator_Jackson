5a131bdf0bb6e037797a0873b8cd8a977f23b5ac
Hunter Jackson <hunter_jackson36@mymail.eku.edu>
2026-01-27 11:26:27 -0500

    Initial Commit

---
+++ M2Calculator_Jackson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2Calculator_Jackson
+//
+//  Created by Jackson, Hunter B. on 1/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
